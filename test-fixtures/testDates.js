2f3fd09f130a2a922da62a8c8aeaa069980c1a10
Ken Snyder <kendsnyder@gmail.com>
2020-10-10 22:57:55 -0600

    [master] cleanup

--- test-fixtures/testDates.js
+++ test-fixtures/testDates.js
@@ -2,19 +2,6 @@ const { DateTime, FixedOffsetZone } = require('luxon');
 const localeList = require('./localeList.js');
 const parser = require('../index.js');
 
-// function testDates({ name, expected, locales, dates }) {
-// 	describe(name, () => {
-// 		locales.forEach(locale => {
-// 			dates.forEach(date => {
-// 				it(`should handle "${date}" (${locale})`, () => {
-// 					const actual = parser.attempt(date);
-// 					expect(actual).toEqual(expected);
-// 				});
-// 			});
-// 		});
-// 	});
-// }
-
 function testDates({ name, formats, expected, locales }) {
 	for (const locale of locales || localeList) {
 		describe(`${name} (${locale})`, () => {
